dc40d109f87b0f9b804710bab0b4ec2872585c05
Vincent Bui <vincent.bui657@gmail.com>
2023-07-24 18:33:33 +0000

    Completed "Combine Arrays"

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,23 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+    // Calculate the lengths of the input arrays
+    int length1 = arr1.length;
+    int length2 = arr2.length;
 
-        return null;
+    // Create a new array to store the combined elements
+    int[] combinedArray = new int[length1 + length2];
+
+    // Copy elements from arr1 to the combinedArray
+    for (int i = 0; i < length1; i++) {
+    combinedArray[i] = arr1[i];
+    }
+
+    // Copy elements from arr2 to the combinedArray
+    for (int i = 0; i < length2; i++) {
+    combinedArray[length1 + i] = arr2[i];
+    }
+
+    return combinedArray;
     }
 }
